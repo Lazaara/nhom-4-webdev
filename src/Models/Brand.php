02786d83c9a97cc124503a68c7ebbf0a481f642a
Đỗ Hồng Thoại <hongthoai69420@gmail.com>
2026-03-12 00:39:09 +0700

    Update Brand.php

---
+++ src/Models/Brand.php
@@ -0,0 +1,33 @@
+<?php
+class Brand
+{
+    private int $id = 0;
+    private string $name = null;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
+?>
